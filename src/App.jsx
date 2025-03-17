5e956ef1035f10c019028fa043a1958b89958c5b
unknown <kriyonainfotech@gmail.com>
2025-03-17 19:18:55 +0530

    save

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import "./App.css";
 import ACDBResidential from "./ACDB_Residential";
 import Header from "./Header";
 import DCDBFuseSPD from "./DCDB_fuse_SPD";
-import DCDB_MCB_SPD from "./DCDB_MCBSPD.JSX";
 import DCDB_FUSE_MCB_SPD from "./DCDB(FUSE+MCB+SPD)";
 import BOSKitTable from "./BOSKitTable";
+import DCDB_MCB_SPD from "./DCDB_MCB_SPD";
 
 function App() {
   return (
