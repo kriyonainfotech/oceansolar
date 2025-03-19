95df739c3ba968d00bf359f5eb9e781f02f36b1c
unknown <kriyonainfotech@gmail.com>
2025-03-19 17:17:10 +0530

    first commit

--- src/ACDB_C1.jsx
+++ src/ACDB_C1.jsx
@@ -1,4 +1,4 @@
-const ACDBTable = () => {
+const ACDBTable1 = () => {
   const tableData = [
     {
       orderCode: "A.5-15.1IIO.AOS",
@@ -117,7 +117,7 @@ const ACDBTable = () => {
         Commercial & Industrial
       </h2>
       <h2 className="text-2xl font-bold mb-4">ACDB (Without No Volt Relay)</h2>
-     
+
       <div className="overflow-x-auto rounded-lg border">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
@@ -181,4 +181,4 @@ const ACDBTable = () => {
   );
 };
 
-export default ACDBTable;
+export default ACDBTable1;
--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import DCDBFuseSPD from "./DCDB_fuse_SPD";
 import DCDB_FUSE_MCB_SPD from "./DCDB(FUSE+MCB+SPD)";
 import BOSKitTable from "./BOSKitTable";
 import DCDB_MCB_SPD from "./DCDB_MCB_SPD";
-// import ACDBTable1 from "./ACDB_C1.JSX";
 import ACDBTable2 from "./ACDB_C2";
 import DCDBTable1 from "./DCDB_C1.JSX";
 import DCDB_LAST from "./DCDB_LAST";
 import Footer from "./Footer";
 import ScrollToTop from "./ScrollToTop";
-import ACDBTable from "./ACDB_C1";
+import ACDBTable1 from "./ACDB_C1";
 
 function App() {
   return (
@@ -23,7 +22,7 @@ function App() {
       <DCDB_MCB_SPD />
       <DCDB_FUSE_MCB_SPD />
       <BOSKitTable />
-      <ACDBTable />
+      <ACDBTable1 />
       <ACDBTable2 />
       <DCDBTable1 />
       <DCDB_LAST />
